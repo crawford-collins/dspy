2e584ff0f3ac18811f98526bdaa5316aa8f5623b
crawford-collins <crawford.collins@springventuregroup.com>
2025-09-26 14:58:32 -0500

    fixed spacing

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 try:
     with open("README.md", "r", encoding="utf-8") as fh:
         long_description = fh.read()
-  except FileNotFoundError:
+except FileNotFoundError:
     long_description = "DSPy fork: The framework for programming—not prompting—foundation models"
 
 setup(
